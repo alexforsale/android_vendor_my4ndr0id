13a3dd1ce54e234ef5350562a98d049304796127
alexforsale <alexforsale@yahoo.com>
2014-05-31 23:48:15 +0700

    add aokp drm

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -2,7 +2,8 @@
 PRODUCT_PACKAGES += \
        ThemeManager \
        ThemeChooser \
-       com.tmobile.themes
+       com.tmobile.themes \
+       Achromatic
 
 PRODUCT_COPY_FILES += \
        vendor/my4ndr0id/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
